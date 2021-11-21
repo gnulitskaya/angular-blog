7872443310b2c005443b1f6ee75250bd3aa0a3c8
Flashy <khoruzhenko.work@gmail.com>
2021-11-21 20:20:16 +0300

    add exp token

--- src/app/admin/shared/components/admin-layout/services/auth.service.ts
+++ src/app/admin/shared/components/admin-layout/services/auth.service.ts
@@ -1,24 +1,36 @@
-import { User } from './../../../../../shared/components/interfaces';
+import { environment } from './../../../../../../environments/environment';
+import { User, FbAuthResponse } from './../../../../../shared/interfaces';
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 @Injectable()
 
 export class AuthService {
   constructor(private http: HttpClient) {}
 
-  get token(): string {
-    return ''
+  get token(): string | null {
+    const storedExpDate = localStorage.getItem('fb-token-exp');
+    const expDate: Date | null = storedExpDate != null ? new Date(storedExpDate) : null;
+    if (expDate == null || new Date() > expDate) {
+      this.logout()
+      return null;
+    }
+    return localStorage.getItem('fb-token');
   }
 
   login(user: User): Observable<any> {
+    user.returnSecureToken = true
     //post запрос чтобы авторизоваться
-    return this.http.post('', user)
+    return this.http.post<FbAuthResponse>(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.apiKey}`, user)
+      .pipe (
+        tap(this.setToken)
+      )
   }
 
   logout() {
-
+    this.setToken(null)
   }
 
   //авторизован пользователь или нет
@@ -27,7 +39,17 @@ export class AuthService {
   }
 
   //чтобы изменять токен
-  private setToken() {
-
+  private setToken(response: FbAuthResponse | null) {
+      // console.log(response)
+    if (response) {
+      //дата когда токен истечёт
+      //приходит 3600s, нужно мили
+      const expDate = new Date(new Date().getTime() + +response.expiresIn * 1000)
+      //сохранили данные токена в localstorage
+      localStorage.setItem('fb-token', response.idToken)
+      localStorage.setItem('fb-token-exp', expDate.toString())
+    } else {
+      localStorage.clear()
+    }
   }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,11 @@
+import { Environment } from './interface';
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-export const environment = {
-  production: false
+export const environment: Environment = {
+  production: false,
+  apiKey: 'AIzaSyDOKAB0X6d1tl_3BLQKwU0CR3EIxDpEI1A'
 };
 
 /*
