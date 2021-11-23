4d63ea2cf9d3906533319bb5eb0f7cb429f145b7
V.Gnulitskaya <v.gnulitskaya@finflowsoft.com>
2021-11-23 17:55:37 +0300

    edit create-page

--- src/app/shared/interfaces.ts
+++ src/app/shared/interfaces.ts
@@ -8,3 +8,11 @@ export interface FbAuthResponse {
   idToken: string
   expiresIn: string
 }
+
+export interface Post {
+  id?: string
+  title: string
+  text: string
+  author: string
+  date: Date
+}
