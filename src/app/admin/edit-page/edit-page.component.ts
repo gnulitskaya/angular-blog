489d1cc172263870d832e2bf2b26f2b76e4eb123
V.Gnulitskaya <v.gnulitskaya@finflowsoft.com>
2021-11-26 19:31:59 +0300

    edit post

--- src/app/admin/edit-page/edit-page.component.ts
+++ src/app/admin/edit-page/edit-page.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Params} from "@angular/router";
+import {PostsService} from "../../shared/posts.service";
+import {switchMap} from "rxjs/operators";
+import {Post} from "../../shared/interfaces";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-edit-page',
@@ -6,10 +11,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-page.component.scss']
 })
 export class EditPageComponent implements OnInit {
-
-  constructor() { }
+  form?: FormGroup
+  constructor(private route: ActivatedRoute,
+              private postsService: PostsService) { }
 
   ngOnInit(): void {
+    //подпишемся на стрим текущего роута чтобы получить перам
+    this.route.params
+      .pipe(switchMap((params: Params) => {
+        return this.postsService.getById(params['id'])
+      })).subscribe((post: Post) => {
+        //инициализировать форму которая позволит менять данные поста
+      this.form = new FormGroup({
+        title: new FormControl(post.title, Validators.required),
+        text: new FormControl(post.text, Validators.required)
+      })
+    })
   }
 
+  submit() {
+
+  }
 }
