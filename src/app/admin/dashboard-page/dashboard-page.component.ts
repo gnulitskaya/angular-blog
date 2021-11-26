9dc366ae9757b0c9550c5632bef883ddaf837104
V.Gnulitskaya <v.gnulitskaya@finflowsoft.com>
2021-11-26 14:24:45 +0300

    show posts on dashboard-page

--- src/app/admin/dashboard-page/dashboard-page.component.ts
+++ src/app/admin/dashboard-page/dashboard-page.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {PostsService} from "../../shared/posts.service";
+import {Post} from "../../shared/interfaces";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-dashboard-page',
   templateUrl: './dashboard-page.component.html',
   styleUrls: ['./dashboard-page.component.scss']
 })
-export class DashboardPageComponent implements OnInit {
-
-  constructor() { }
+export class DashboardPageComponent implements OnInit, OnDestroy{
+  posts: Post[] = []
+  pSub?: Subscription
+  constructor(private postsService: PostsService) { }
 
   ngOnInit(): void {
+    this.postsService.getAll().subscribe(posts => {
+      this.posts = posts
+    })
   }
 
+  ngOnDestroy(): void {
+    if(this.pSub) {
+      this.pSub.unsubscribe()
+    }
+  }
+
+  remove(id: any) {
+
+  }
 }
